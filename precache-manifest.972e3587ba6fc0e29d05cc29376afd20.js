052b4ec67046477803cb0188ad70194e6ac95864
OleksandrHlybchenko <logista@meta.ua>
2019-08-05 07:50:49 +0300

    Updates

--- precache-manifest.972e3587ba6fc0e29d05cc29376afd20.js
+++ precache-manifest.972e3587ba6fc0e29d05cc29376afd20.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "323cb58d2b4a8ec9ddbd84b8e47124b2",
+    "revision": "fb7894891115674ece1c646f8cb3210a",
     "url": "/react_phone-catalog/index.html"
   },
   {
-    "revision": "6e22b3fd073ab8ae05f4",
-    "url": "/react_phone-catalog/static/css/main.12d235cd.chunk.css"
+    "revision": "54a172c32537d4473b50",
+    "url": "/react_phone-catalog/static/css/main.c1da50bf.chunk.css"
   },
   {
     "revision": "084ce024c73de484f666",
     "url": "/react_phone-catalog/static/js/2.436c20d0.chunk.js"
   },
   {
-    "revision": "6e22b3fd073ab8ae05f4",
-    "url": "/react_phone-catalog/static/js/main.d64019c4.chunk.js"
+    "revision": "54a172c32537d4473b50",
+    "url": "/react_phone-catalog/static/js/main.bedf4798.chunk.js"
   },
   {
     "revision": "9b3e1d68b9cc1dd9a925",
