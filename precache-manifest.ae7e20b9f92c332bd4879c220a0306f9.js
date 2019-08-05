ad26d08d1b75ee59994d88a45112b19a269fae5b
OleksandrHlybchenko <logista@meta.ua>
2019-08-05 06:58:37 +0300

    Updates

---
+++ precache-manifest.ae7e20b9f92c332bd4879c220a0306f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21104fb456fd88afe48fd38eefc93f49",
+    "url": "/phone_catalog/index.html"
+  },
+  {
+    "revision": "6e22b3fd073ab8ae05f4",
+    "url": "/phone_catalog/static/css/main.12d235cd.chunk.css"
+  },
+  {
+    "revision": "084ce024c73de484f666",
+    "url": "/phone_catalog/static/js/2.436c20d0.chunk.js"
+  },
+  {
+    "revision": "6e22b3fd073ab8ae05f4",
+    "url": "/phone_catalog/static/js/main.d64019c4.chunk.js"
+  },
+  {
+    "revision": "a54d5f86aabe9392a534",
+    "url": "/phone_catalog/static/js/runtime~main.a1d9a81b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/phone-catalog/precache-manifest.35edfe65851654a1dd5d086841957245.js"
+  "/phone_catalog/precache-manifest.ae7e20b9f92c332bd4879c220a0306f9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/phone-catalog/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/phone_catalog/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
